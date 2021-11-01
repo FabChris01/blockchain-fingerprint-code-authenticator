ec753d5db3d4e20232ab8133b9003fe581c9bc58
Fabian Christopher <54461744+FabChris01@users.noreply.github.com>
2021-11-01 11:43:22 +0530

    Added Json Blockchain fetch data

--- gamma_app/views.py
+++ gamma_app/views.py
@@ -2,30 +2,56 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+import datetime
 
 from Crypto.PublicKey import RSA
 from hashlib import sha512
 
-validator=['3wfeevg3g']
+global block
+# block = {
+#     'timestamp',
+#     'signature',
+#     'hash',
+#     'keyPair',
+# }
+
+block = set()
+
+validator=['3wfeevg3g','d3wg678g','cadw34as']
 keyPair = RSA.generate(bits=1024)
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, World")
 
-def encrypt(data):
-    enc_data=data.encode("utf8")
-    hashA = int.from_bytes(sha512(enc_data).digest(), byteorder='big')
-    signature = pow(hashA, keyPair.d, keyPair.n)
-    encoded_val=validator[0].encode("utf8")
-    check=decrypt(signature, encoded_val)
-    return check
 
 def decrypt(data, original):
     hashB = int.from_bytes(sha512(original).digest(), byteorder='big')
     hashFromSignature = pow(data, keyPair.e, keyPair.n)
     checkVal = hashB == hashFromSignature
     return(checkVal)
+      
+def encrypt(data):
+    enc_data=data.encode("utf8")
+    hashA = int.from_bytes(sha512(enc_data).digest(), byteorder='big')
+    signature = pow(hashA, keyPair.d, keyPair.n)
+    encoded_val=validator[0].encode("utf8")
+    check=decrypt(signature, encoded_val)
+    # block = create_block(hashA, signature, keyPair)
+    return (check)
+
+def create_block(keyPair, data):
+    enc_data=data.encode("utf8")
+    hashA = int.from_bytes(sha512(enc_data).digest(), byteorder='big')
+    signature = pow(hashA, keyPair.d, keyPair.n)
+    block = {
+        "timestamp": str(datetime.datetime.now()),
+        "hash_value": hashA,
+        "encoded_signature": signature,
+        # "keyPair": keyPair,
+    }
+    return block
+
 
 @csrf_exempt
 def get_data(request):
@@ -33,9 +59,10 @@ def get_data(request):
         received_json = json.loads(request.body)
         frontend_data = received_json['fingerprint']
         final_answer = encrypt(frontend_data)
+        final_block=create_block(keyPair, frontend_data)
         if(final_answer==True):
-            return JsonResponse({"message": "Authentication Successful"})
+            return JsonResponse({"message": "Authentication Successful", 'data': json.dumps(final_block)})
         else:
-            return JsonResponse({"message": "Authentication Failed"})
+            return JsonResponse({"message": "Authentication Failed",})
     return JsonResponse({"message": "error"})
 
